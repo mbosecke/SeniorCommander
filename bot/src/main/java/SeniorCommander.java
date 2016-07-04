db579f3a491dd486f50c2263f5b2fb4fe18c25be
Mitchell Bösecke <mitchellbosecke@gmail.com>
2016-07-03 20:46:17 -0600

    Added gradle structure

---
+++ bot/src/main/java/SeniorCommander.java
@@ -0,0 +1,9 @@
+/**
+ * Created by mitch_000 on 2016-07-03.
+ */
+public class SeniorCommander {
+
+    public static void main(String[] args){
+        System.out.println("Hello World.");
+    }
+}
