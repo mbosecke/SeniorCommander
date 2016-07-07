9a168fd8d8cf78b68309e33bf88da339e26e9fe8
Mitchell Bösecke <mitchellbosecke@gmail.com>
2016-07-06 23:26:24 -0600

    Added a cooldown

--- bot/src/main/java/com/mitchellbosecke/seniorcommander/handler/DiceHandler.java
+++ bot/src/main/java/com/mitchellbosecke/seniorcommander/handler/DiceHandler.java
@@ -5,6 +5,7 @@ import com.mitchellbosecke.seniorcommander.message.Message;
 import com.mitchellbosecke.seniorcommander.message.MessageHandler;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,15 +16,26 @@ public class DiceHandler implements MessageHandler {
 
     private final Pattern pattern = Pattern.compile("!d(\\d+)");
 
+    private final Cooldown cooldown = new Cooldown(1, TimeUnit.MINUTES);
+
     @Override
     public void handle(Context context, Message message) {
         Matcher matcher = pattern.matcher(message.getContent());
         if (matcher.matches()) {
-            Integer num = Integer.valueOf(matcher.group(1));
-            Random generator = new Random();
-            int result = generator.nextInt(num) + 1;
 
-            context.getMessageQueue().add(Message.response(message, "You rolled a " + result));
+            String user = message.getUser();
+
+            if (cooldown.isReady(user)) {
+                Integer num = Integer.valueOf(matcher.group(1));
+                Random generator = new Random();
+                int result = generator.nextInt(num) + 1;
+
+                context.getMessageQueue().add(Message.response(message, "You rolled a " + result + "."));
+                cooldown.reset(user);
+            } else {
+                context.getMessageQueue().add(Message.response(message, "Son, you need to slow down. " + cooldown
+                        .secondsRemaining(message.getUser()) + " seconds remaining."));
+            }
         }
     }
 }
