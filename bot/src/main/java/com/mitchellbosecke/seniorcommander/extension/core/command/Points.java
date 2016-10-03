cb8959698c896b201386bafdb271cadc13917095
Mitchell Bösecke <mitchellbosecke@gmail.com>
2016-10-02 20:21:16 -0600

    Added a community setting to change the name of points

--- bot/src/main/java/com/mitchellbosecke/seniorcommander/extension/core/command/Points.java
+++ bot/src/main/java/com/mitchellbosecke/seniorcommander/extension/core/command/Points.java
@@ -1,6 +1,7 @@
 package com.mitchellbosecke.seniorcommander.extension.core.command;
 
 import com.mitchellbosecke.seniorcommander.CommandHandler;
+import com.mitchellbosecke.seniorcommander.domain.CommunityModel;
 import com.mitchellbosecke.seniorcommander.domain.CommunityUserModel;
 import com.mitchellbosecke.seniorcommander.extension.core.service.UserService;
 import com.mitchellbosecke.seniorcommander.message.Message;
@@ -23,6 +24,11 @@ public class Points implements CommandHandler {
 
     private final MessageQueue messageQueue;
 
+    private static final String DEFAULT_POINT_SINGULAR = "point";
+    private static final String DEFAULT_POINT_PLURAL = "points";
+    private static final String SETTING_POINT_SINGULAR = "points.singular";
+    private static final String SETTING_POINT_PLURAL = "points.plural";
+
     public Points(MessageQueue messageQueue, UserService userService) {
         this.messageQueue = messageQueue;
         this.userService = userService;
@@ -32,17 +38,22 @@ public class Points implements CommandHandler {
     public void execute(Message message) {
         ParsedCommand parsed = new CommandParser().parse(message.getContent());
 
+        CommunityModel community = userService.findCommunity(message.getChannel());
+
+        String pointsName = community.getSetting(SETTING_POINT_PLURAL);
+        pointsName = pointsName == null ? DEFAULT_POINT_PLURAL : pointsName;
+
         if (parsed.getComponents().isEmpty()) {
             CommunityUserModel user = userService.findUser(message.getChannel(), message.getSender());
-            messageQueue.add(Message.response(message, String.format("you have %d points.", user.getPoints())));
+            messageQueue.add(Message.response(message, String.format("you have %d %s.", user.getPoints(), pointsName)));
         } else {
             CommunityUserModel user = userService.findUser(message.getChannel(), parsed.getComponents().get(0));
 
             if (user == null) {
                 messageQueue.add(Message.response(message, "username not found"));
             } else {
-                messageQueue
-                        .add(Message.response(message, String.format("%s has %d points.", user.getName(), user.getPoints())));
+                messageQueue.add(Message.response(message, String
+                        .format("%s has %d %s.", user.getName(), user.getPoints(), pointsName)));
             }
         }
 
